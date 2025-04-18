4d4dae2b6358ef9bcbd3a6aca594acb6d51ef8e9
Astrid La Reine Lockhart <astrid.lr.lockhart@gmail.com>
2025-04-18 17:45:30 -0500

    Update localIPcheck.sh

--- localIPcheck.sh
+++ localIPcheck.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 # meant to be built into a script that can scan a network for valid IPs
-# Will add a more easily read list and give functionality to modify how many subnets it will scan.
 declare -i ipEndOctet=1
+deviceList=""
 
-while [ $ipEndOctet -le 255 ]; do
-        ping -c 1 192.168.1.$ipEndOctet;
-        ipEndOctet=$((ipEndOctet + 1))
+echo "Map of detectable devices on the current local network. "
+echo "--------------------------------------------------------"
+while [ $ipEndOctet -le 255 ]; do # Scans up to 255. Change the integer in the condition to change how many devices on the subnet are pinged.
+	ping -c 1 192.168.1.$ipEndOctet > /dev/null 2>&1;
+	if [ $? -ne 0 ]; then
+		echo "scan result: 192.168.1.$ipEndOctet not detected"
+	else
+		deviceList+="192.168.1.$ipEndOctet\\n"
+		echo "scan result 192.168.1.$ipEndOctet detected"
+	fi
+	ipEndOctet=$((ipEndOctet + 1))
 done
+echo "----------------"
+echo "List of devices:"
+echo "----------------"
+echo -e $deviceList;
+
 
